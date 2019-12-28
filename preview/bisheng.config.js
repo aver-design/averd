0e4e3cb36e200d98feaee0a1bda020fc1fcd0434
GuessEver <guessever@gmail.com>
2019-12-28 12:41:16 +0800

    chore(preview): apply preview custom template html

--- preview/bisheng.config.js
+++ preview/bisheng.config.js
@@ -2,6 +2,7 @@ module.exports = {
   port: 8077,
   source: './components',
   output: './preview/dist',
+  htmlTemplate: './preview/theme/static/index.html',
   theme: './preview/theme',
   themeConfig: {},
 };
